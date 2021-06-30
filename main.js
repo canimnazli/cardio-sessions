59ddfa0df42c82ac96dd9896ca1efe395a4ddb36
canimnazli <83221896+canimnazli@users.noreply.github.com>
2021-06-30 15:30:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ class UI {
   static showAlert(message, className) {
     const div = document.createElement('div');
     div.className = `alert alert-${className}`;
-    div.appendChild(document.createTextNode(message));
+    div.append(document.createTextNode(message));
     const container = document.querySelector('.container');
     const form = document.querySelector('#book-form');
     container.insertBefore(div, form);
